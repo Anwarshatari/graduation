a574498009ec20d5fcae1f2878a08ecc3f776991
Anwarshatari <shatarimarket2@gmail.com>
2016-12-08 19:13:29 +0300

    تشغيل حساس الحرارة

--- sketch_1/sketch_1.ino
+++ sketch_1/sketch_1.ino
@@ -1,18 +1,35 @@
+#include <SimpleDHT.h>
 
-// تشغيل أول حساس 
+int pinDHT11 = 2; // pin number on board 
+SimpleDHT11 dht11; // dh11 object from class
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  Serial.println("_____________________________");
+ 
+  Serial.println("Temprature and humidity  readings ");
+  
+  byte temperature = 0;
+  byte humidity = 0;
 
+  if (dht11.read(pinDHT11, &temperature, &humidity, NULL)) {
 
+    Serial.print("sensor readings fail");
+    
+    return;
+  }
+  
 
+  Serial.print((int)temperature); Serial.print(" C "); 
+  Serial.print((int)humidity); Serial.println("%");
+  
+
+  delay(2000);
+}
 
 // نهاية الكود
-
-// نور شطاري
-}
+//  أنور شطاري
